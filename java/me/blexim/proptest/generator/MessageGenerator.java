293db438a092ca0b4811fdfa8bd190243d672f69
Matt <matt@cantab.net>
2017-11-20 23:48:29 +0000

    Fix tests

--- java/me/blexim/proptest/generator/MessageGenerator.java
+++ java/me/blexim/proptest/generator/MessageGenerator.java
@@ -39,6 +39,18 @@ public class MessageGenerator<T extends Message> implements Generator<T> {
     }
   }
 
+  public static <T extends Message> MessageGenerator<T> create(Class<T> clazz) {
+    try {
+      GenericGenerator genericGenerator = GenericGeneratorImpl.create();
+      ProtobufGenerator protobufGenerator = ProtobufGenerator.create(genericGenerator);
+      T defaultInstance = (T) clazz.getMethod("getDefaultInstance").invoke(null);
+      return create(defaultInstance, protobufGenerator, genericGenerator);
+    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+      logger.error("Couldn't get default message instance", e);
+      return null;
+    }
+  }
+
   @Override
   public T next(Random rand) {
     try {
